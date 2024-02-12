94a5d25f6251bdca982ebbf3c4e6a128046effad
Pranita <pranitapatil1711@gmail.com>
2024-02-12 19:12:21 +0530

    react website

---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+const Hero = () =>
+{
+    return <main className="hero container">
+
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FFET DESERVE THE BEST AND WHERE TO HELP YOU WITH OUR 
+                SHOES. YOUR FFET DESERVE THE BEST AND WHERE TO HELP YOU WITH OUR 
+                SHOES.
+            </p>
+
+<div className="hero-btn">
+    <button>Shop Now</button>
+    <button className="secondary-btn">Category</button>
+</div>
+
+
+<div className="shopping">
+    <p>Also Avaliable on</p>
+
+    <div className="brand-icons">
+        <img src="/images/amazon.png"/>
+        <img src="/images/flipkart.png"/>
+    </div>
+</div>
+
+</div>
+
+
+        <div className="hero-image">
+           <img src="/images/shoe_image.png" />
+        </div>
+    </main>
+
+};
+export default Hero;
